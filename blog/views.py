75a74f30469498a3b2b71b731d4f52ed01b0b802
tossynfaw <tossyn02@yahoo.com>
2016-06-09 13:59:46 +0100

    Error corrected in view.py line 45 - "Blog/post_edit.html" changed to blog/post_edit.html. It worked perfectly on dev env but failed on server windows and linux differences

--- blog/views.py
+++ blog/views.py
@@ -42,4 +42,4 @@ def post_edit(request, pk):
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance = post)
-	return render(request, 'Blog/post_edit.html', {'form': form})
+	return render(request, 'blog/post_edit.html', {'form': form})
